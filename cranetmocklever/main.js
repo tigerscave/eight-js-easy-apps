724d4c1d88ef6d51aeec24b3a68bc81415b2819e
yuto <y.yatsumimi@gmail.com>
2023-05-24 18:45:56 +0900

    add-disable-button

--- cranetmocklever/main.js
+++ cranetmocklever/main.js
@@ -6,27 +6,37 @@ const leverList2 = document.getElementById('lever-list-2');
 const leverList3 = document.getElementById('lever-list-3');
 const leverList4 = document.getElementById('lever-list-4');
 const leverList5 = document.getElementById('lever-list-5');
+const groupSelectItems = document.querySelectorAll(".groups-select-item")
 
-const leverLists = {
-  leverList2: document.getElementById('lever-list-2'),
-  leverList3: document.getElementById('lever-list-2'),
-  leverList4: document.getElementById('lever-list-2'),
-  leverList5: document.getElementById('lever-list-2'),
-};
+//カスタム編集ボタンを使う宣言。edit-button
+let isDisable = true
+// レバー設定ボタン・ペダル設定ボタンが有効になる。
+editButton.addEventListener('click', () => {
+  if (isDisable) {
+    groupSelectItems.forEach(item => {
+      item.removeAttribute('disabled')
+    })
+    editButton.textContent = 'カスタム設定を保存';
+    isDisable = false
+  } else {
+    groupSelectItems.forEach(item => {
+      item.setAttribute('disabled', null)
+    })
+    editButton.textContent = 'カスタム設定を編集';
+    isDisable = true
+  }
+});
+leverList2.addEventListener('change', checkSelection);
+leverList3.addEventListener('change', checkSelection);
+leverList4.addEventListener('change', checkSelection);
+leverList5.addEventListener('change', checkSelection);
 
-leverList4.addEventListener('change', () => {
-  if (leverList4.value === '未選択') {
+function checkSelection() {
+  if (leverList2.value === '未選択' || leverList3.value === '未選択' || leverList4.value === '未選択' || leverList5.value === '未選択') {
     editButton.setAttribute('disabled', '');
   } else {
-  editButton.removeAttribute('disabled');
+    editButton.removeAttribute('disabled');
+  }
 }
-});
 
-leverList2.addEventListener('change', () => {
-  if (leverList2.value === '未選択') {
-    editButton.setAttribute('disabled', '');
-  } else {
-  editButton.removeAttribute('disabled');
-}
-});
 
