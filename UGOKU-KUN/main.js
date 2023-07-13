bef233672aefa0cf4eefcc9cf07a4ab0387e622c
yuto <y.yatsumimi@gmail.com>
2023-07-13 15:11:50 +0900

    refactor:button-color

--- UGOKU-KUN/main.js
+++
@@ -1,8 +0,0 @@
-const stopSwitch = document.getElementById("stop-switch");
-stopSwitch.addEventListener("touchstart", () => {
-  stopSwitch.style.backgroundColor = 'red';
-});
-stopSwitch.addEventListener("mouseup", () => {
-  stopSwitch.style.backgroundColor = 'lightpink';
-});
-
