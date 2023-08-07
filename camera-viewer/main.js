2868ffa811445f2b4fe39e624e7634e244a462d9
yuto <y.yatsumimi@gmail.com>
2023-08-07 19:18:44 +0900

    rename:tag

--- camera-viewer/main.js
+++ camera-viewer/main.js
@@ -1,17 +1,17 @@
 'use strict'
 
-const setIpAddress = document.getElementById("ip-address")
+const ipAddressInputElement = document.getElementById("ip-address")
 const registerIpAddressButton = document.getElementById("register-ip-address-button")
 const cameraViewer = document.getElementById("camera-viewer")
 
 registerIpAddressButton.addEventListener('click',()=>{
   if(registerIpAddressButton.innerText === "編集"){
     registerIpAddressButton.innerText = "保存";
-    setIpAddress.disabled = false;
+    ipAddressInputElement.disabled = false;
   } else if(registerIpAddressButton.innerText === "保存"){
     registerIpAddressButton.innerText = "編集";
     //入力されたIPアドレスの値を、カメラビューワーに反映させる。
-    cameraViewer.src = "http://" + setIpAddress.value + "/ImageViewer?Mode=Motion&Resolution=640x360&Quality=Standard&Interval=10";
-    setIpAddress.disabled = true;
+    cameraViewer.src = "http://" + ipAddressInputElement.value + "/ImageViewer?Mode=Motion&Resolution=640x360&Quality=Standard&Interval=10";
+    ipAddressInputElement.disabled = true;
   }
 });
