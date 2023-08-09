e1948ae4fc823a7fbed91a470dfae51497ed0401
yuto <y.yatsumimi@gmail.com>
2023-08-09 17:22:18 +0900

    add:preset and ratio

--- camera-viewer/main.js
+++ camera-viewer/main.js
@@ -40,7 +40,7 @@ registerIpAddressButton.addEventListener('click', () => {
     ipAddressInputElement.disabled = true;
     localStorage.setItem('address-number', ipAddressInputElement.value);
     registerIpAddressButton.style.background = "#D9E5FF";
-    
+
     //文字の切り替え（接続中⇔表示する映像はありません）
     if (localStorage.getItem('address-number') !== "") {
       ipAddressInputElement.value = localStorage.getItem('address-number')
@@ -60,6 +60,49 @@ if (localStorage.getItem('address-number') !== "") {
 } else {
   networkMessage.innerText = "表示する映像はありません。"
 }
+
+//ビューワーのプリセット
+//保存
+const savePreset1Button = document.getElementById('save-preset1')
+const savePreset2Button = document.getElementById('save-preset2')
+const savePreset3Button = document.getElementById('save-preset3')
+
+savePreset1Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camposiset?presetset=1"
+  //画面遷移を防止するため、ページをリロードする。
+  setTimeout(() => {
+    location.reload()
+  }, 200)
+});
+
+savePreset2Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camposiset?presetset=2"
+  setTimeout(() => {
+    location.reload()
+  }, 200)
+});
+
+savePreset3Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/image_adjust?brightness=3"
+  setTimeout(() => {
+    location.reload()
+  }, 200)
+});
+
+//移動
+const movePreset1Button = document.getElementById("move-preset1")
+const movePreset2Button = document.getElementById("move-preset2")
+const movePreset3Button = document.getElementById("move-preset3")
+
+movePreset1Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camctrl?preset=1";
+});
+movePreset2Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camctrl?preset=2";
+});
+movePreset3Button.addEventListener('click', () => {
+  cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camctrl?preset=3";
+});
 
 //ビューワーの拡大縮小
 const zoomInMonitorButton = document.getElementById("zoom-in-monitor-button")
@@ -93,4 +136,18 @@ leftArrowButton.addEventListener('click', () => {
 
 rightArrowButton.addEventListener('click', () => {
   cameraViewer.src = "http://" + ipAddressInputElement.value + "/cgi-bin/camctrl?pan=1&tilt=0&Language=0";
+});
+
+//ビューワーの比率
+const enlargeRatioButton = document.getElementById("enlarge-ratio")
+const reduceRatioButton = document.getElementById("reduce-ratio")
+
+enlargeRatioButton.addEventListener('click', () => {
+  cameraViewer.style.width = '40rem'
+  cameraViewer.style.height = '20rem';
+});
+
+reduceRatioButton.addEventListener('click', () => {
+  cameraViewer.style.width = '30rem'
+  cameraViewer.style.height = '18rem';
 });
